2acf0f526e95976b9a60a2c58a06fa9da86f0fa4
cloudtrov <info@trov.co.in>
2017-06-27 14:40:17 +0530

    Update test.js

--- config/env/test.js
+++ config/env/test.js
@@ -5,7 +5,7 @@
  */
 
 module.exports = {
-  db: 'mongodb://localhost/noobjs_test',
+  db: 'mongodb://vmmongo/noobjs_test',
   facebook: {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
